85254ba472a12268208c751759bae190de5546db
Vinicius-E <viniciuseduardo0702@hotmail.com>
2021-11-15 22:02:09 -0300

    Mapeando as Enumerações na Entidade Lancamento

---
+++ src/main/java/com/projeto/minhasfinancas/model/entity/StatusLancamento.java
@@ -0,0 +1,8 @@
+package com.projeto.minhasfinancas.model.entity;
+
+public enum StatusLancamento {
+
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+}
---
+++ src/main/java/com/projeto/minhasfinancas/model/entity/TipoLancamento.java
@@ -0,0 +1,7 @@
+package com.projeto.minhasfinancas.model.entity;
+
+public enum TipoLancamento {
+
+	RECEITA,
+	DESPESA
+}
