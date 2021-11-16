eee780fa22b1ec7965e001118acc2dc6737d00db
Vinicius-E <viniciuseduardo0702@hotmail.com>
2021-11-15 21:12:19 -0300

    Projeto Criado

---
+++ src/main/java/com/projeto/minhasfinancas/model/entity/Usuario.java
@@ -0,0 +1,10 @@
+package com.projeto.minhasfinancas.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table( name="usuario", schema="financas")
+public class Usuario {
+
+}
